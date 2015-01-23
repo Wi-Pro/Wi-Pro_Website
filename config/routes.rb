f33686c52aa702d9ab870e1874fd23f16a7592af
kevinbird15 <kevinbird15@gmail.com>
2015-01-23 15:46:19 -0600

    Adding the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Myapp::Application.routes.draw do
   get "home/index"
-  get "device/index"
+  get 'device' => 'device#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
