41e603d7c50542d443f2151f3e0e99e9ce434a61
kevinbird15 <kevinbird15@gmail.com>
2015-02-21 01:35:04 -0600

    simplify everything

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -3,7 +3,7 @@ class UploadController < ApplicationController
      render :file => 'app\views\upload\uploadfile.rhtml'
   end
   def uploadFile
-    post = DataFile.save(params[:upload])
+    #post = DataFile.save(params[:upload])
     render :text => "File has been uploaded successfully"
   end
 end
