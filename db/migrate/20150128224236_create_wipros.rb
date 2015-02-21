064fbfcc301c5954c630c63dc4fd0687a60a83ac
kevinbird15 <kevinbird15@gmail.com>
2015-02-21 11:58:49 -0600

    adding a comma

--- db/migrate/20150128224236_create_wipros.rb
+++ db/migrate/20150128224236_create_wipros.rb
@@ -2,7 +2,7 @@ class CreateWipros < ActiveRecord::Migration
   def change
     create_table :wipros do |t|
       t.primary_key, :wpid
-      t.text :wpname
+      t.text, :wpname
       t.timestamps
     end
   end
