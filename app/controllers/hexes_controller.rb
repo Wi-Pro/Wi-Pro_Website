bd20c797746b624add3083d3de1341221399bda0
kevinbird15 <kevinbird15@gmail.com>
2015-02-21 14:48:46 -0600

    Adding hex uploader code

--- app/controllers/hexes_controller.rb
+++ app/controllers/hexes_controller.rb
@@ -1,13 +1,28 @@
 class HexesController < ApplicationController
   def index
+    @hex = Hex.all
   end
 
   def new
+    @hex = Hex.new
   end
 
   def create
+    @hex = Hex.new(hex_params)
+    if @hex.save
+      redirect_to hexes_path, notice: "The hex file #{@hex.name} has been uploaded"
+    else
+      render "new"
+    end
   end
 
   def destroy
+    @hex = Hex.find(params[:id])
+    @hex.destroy
+    redirect_to hexes_path, notice: "The hex file #{@hex.name} has been deleted"
+  end
+private
+  def hex_params
+    params.require(:hex).permit(:name, :attachment)
   end
 end
--- app/models/hex.rb
+++ app/models/hex.rb
@@ -1,2 +1,3 @@
 class Hex < ActiveRecord::Base
+  mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
 end
