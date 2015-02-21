a1fdfac270316da139d63d69f6b0a56f4afb779f
kevinbird15 <kevinbird15@gmail.com>
2015-02-21 01:05:16 -0600

    expanding my routes to explicitly define

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Myapp::Application.routes.draw do
   get "home/index"
   get 'device' => 'device#index'
-  get 'upload'
+  get 'upload' => 'upload#uploadfile'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
