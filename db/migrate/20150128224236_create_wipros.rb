f312138c22e07b5005efd0968f4124f00d2cff20
kevinbird15 <kevinbird15@gmail.com>
2015-02-21 11:47:33 -0600

    Revert "delete WiPro table"
    This reverts commit 259fd62b456df1d37dc1b5e8800b8d864bc7c0be.

---
+++ db/migrate/20150128224236_create_wipros.rb
@@ -0,0 +1,10 @@
+class CreateWipros < ActiveRecord::Migration
+  def change
+    create_table :wipros do |t|
+      t.primary_key, :wpid
+      t.text :wpname
+
+      t.timestamps
+    end
+  end
+end
