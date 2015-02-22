09244f0c4153dc283461c5b66f7e36d4e543733e
kevinbird15 <kevinbird15@gmail.com>
2015-02-22 15:52:14 -0600

    got it working, now filesystem stuff

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -2,10 +2,10 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/0000000000"
   end
 
   def extension_white_list
-    %w(pdf doc htm html docx)
+    %w(hex)
   end
 end
