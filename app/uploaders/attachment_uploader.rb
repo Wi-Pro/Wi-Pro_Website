152de47873ddd0c8a9638601e30b327edce51587
kevinbird15 <kevinbird15@gmail.com>
2015-02-22 19:53:41 -0600

    trying custom filestructure

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -2,7 +2,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/0000000000"
+    "uploads/#{model.class.to_s.underscore}/#{@hex.wiproid}"
   end
 
   def extension_white_list
