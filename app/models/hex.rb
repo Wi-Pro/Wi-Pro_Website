4f80659d6fccd57558971f62f15337607fc2a1f4
kevinbird15 <kevinbird15@gmail.com>
2015-02-22 21:00:10 -0600

    moving location of Gemfile gem

--- app/models/hex.rb
+++ app/models/hex.rb
@@ -1,3 +1,6 @@
 class Hex < ActiveRecord::Base
   mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
+  validates :attachment, presence: true
+  validates :wiproid, presence: true
+  validates :deviceid, presence: true
 end
