63cb7cfb411d5f71f25f9e5544489a862d505b33
kevinbird15 <kevinbird15@gmail.com>
2015-02-24 14:01:41 -0600

    adding post

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Myapp::Application.routes.draw do
   get 'device' => 'device#index'
   get 'hexes/admin' => 'hexes#admin'
   get 'upload' => 'upload#uploadfile.html'
-  post 'hexes/index'
+  post 'hexes'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
