8629ba5a573938d972ea8c224b62989960b59c3c
kevinbird15 <kevinbird15@gmail.com>
2015-02-28 13:57:24 -0600

    creating a controller for adding Wipros to db

---
+++ app/controllers/wiproid_controller.rb
@@ -0,0 +1,32 @@
+class WiproidController < ApplicationController
+  def index
+    @wiproid = Wiproid.new
+  end
+
+  def admin
+    @wiproids = Wiproid.all
+  end
+
+  def new
+    @wiproid = Wiproid.new
+  end
+
+  def create
+    @wiproid = Wiproid.new(wiproid_params)
+    if @hex.save
+      redirect_to wiproid_path, notice: "The WiPro #{@wiproid.name} has been added"
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @wiproid = Wiproid.find(params[:id])
+    @wiproid.destroy
+    redirect_to wiproid_path, notice: "The WiPro #{@hex.name} has been deleted"
+  end
+private
+  def wiproid_params
+    params.require(:wiproid).permit(:name, :wiproid, :userid)
+  end
+end
