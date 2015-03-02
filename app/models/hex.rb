eea15ecd5c20dceed5d157cc0aa1a19b5a57ca3b
kevinbird15 <kevinbird15@gmail.com>
2015-03-01 22:36:27 -0600

    understand now I think

--- app/models/hex.rb
+++ app/models/hex.rb
@@ -1,9 +1,12 @@
+require 'file_size_validator'
 class Hex < ActiveRecord::Base
   mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
-  validates :attachment, presence: true
+  validates :attachment,
+    presence: true,
+    :file_size => {
+      :maximum => 64.kilobytes.to_i
+    }
   validates :wiproid, presence: true
   validates :deviceid, presence: true
-  validates :file_size => {
-    :maximum => 64.kilobytes.to_i
-  }
+
 end
