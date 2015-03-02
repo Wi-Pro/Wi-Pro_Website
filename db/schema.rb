c2d392daac925719654c7058a5cfd80d4ed72ef0
root <kevinbird15@gmail.com>
2015-03-02 16:14:47 -0600

    making user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225184243) do
+ActiveRecord::Schema.define(version: 20150302221200) do
 
   create_table "hex_files", force: true do |t|
     t.datetime "created_at"
@@ -38,11 +38,8 @@ ActiveRecord::Schema.define(version: 20150225184243) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "name"
     t.string   "email"
-    t.integer  "userid"
-    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
