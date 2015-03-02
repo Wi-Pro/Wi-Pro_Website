778fd5bbb2ebe71d5f7ee1b7dc69883eaf69f9d4
kevinbird15 <kevinbird15@gmail.com>
2015-03-02 15:24:15 -0600

    Adding deviceids controller more to come later

---
+++ app/controllers/deviceids_controller.rb
@@ -0,0 +1,32 @@
+class DeviceidsController < ApplicationController
+  def index
+    @wiproid = Wiproid.new
+  end
+
+  def admin
+    @wiproids = Wiproid.all
+  end
+
+  def new
+    @wiproid = Wiproid.new
+  end
+
+  def create
+    @wiproid = Wiproid.new(wiproid_params)
+    if @wiproid.save
+      redirect_to "/hexes", notice: "The WiPro #{@wiproid.name} has been added"
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @wiproid = Wiproid.find(params[:id])
+    @wiproid.destroy
+    redirect_to wiproids_path, notice: "The WiPro #{@wiproid.name} has been deleted"
+  end
+private
+  def wiproid_params
+    params.require(:wiproid).permit(:name, :wiproid, :userid)
+  end
+end
