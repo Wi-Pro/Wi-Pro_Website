b6d184a383f8e03f96f63895ccad5d0cec47295b
kevinbird15 <kevinbird15@gmail.com>
2015-03-04 15:31:33 -0600

    adding some feedback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
 
     else
@@ -19,5 +19,12 @@ class UsersController < ApplicationController
 
   def index
     redirect_to "/"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
