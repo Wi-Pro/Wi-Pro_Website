0109f02b18bbb194a3d78f7e8483cf03e2909dc3
kevinbird15 <kevinbird15@gmail.com>
2015-03-04 22:13:53 -0600

    adding flash alerts to every page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome!"
+      flash[:success] = "Welcome #{@user.name}!"
       redirect_to "/"
     else
       render 'new'
