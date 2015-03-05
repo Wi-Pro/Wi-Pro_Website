0391df947a47ceb333c96eb63183a41e8b6b2776
kevinbird15 <kevinbird15@gmail.com>
2015-03-04 23:15:43 -0600

    normalizing wiproid to 10 digits

--- app/models/wiproid.rb
+++ app/models/wiproid.rb
@@ -1,2 +1,5 @@
 class Wiproid < ActiveRecord::Base
+  validates :name, presence: true
+  validates :wiproid, presence: true, length: {is: 10} #wiproid must be 10 digits long
+  validates :userid, presence: true
 end
