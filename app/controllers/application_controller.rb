ff7c8cb139399cdfda46e800a5c2bd56a7e775fc
kevinbird15 <kevinbird15@gmail.com>
2015-03-05 15:44:39 -0600

    adding some helpers and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,24 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :log_in
-  
+  helper_method :current_user
+  helper_method :logged_in?
+  helper_method :log_out
+
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to root_url
   end
 
-  
+
 end
