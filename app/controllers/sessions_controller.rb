a9ee2d91e342a5b78bfdb5786b3d9861fcb2b224
kevinbird15 <kevinbird15@gmail.com>
2015-03-04 22:59:44 -0600

    Adding user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[session][:password])
+      flash[:success] = "Welcome Back!"
+      redirect_to "/hexes"
+    else
+      flash.now[:alert] = "Invalid Email/Password"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
