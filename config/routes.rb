fd320fe3f70a5b70af3c1640e4ac1a23d23083a6
kevinbird15 <kevinbird15@gmail.com>
2015-03-06 03:27:54 -0600

    get rid of homepage

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Myapp::Application.routes.draw do
   #resources :hexes, only: [:index, :new, :create, :destroy]
   #resources :wiproids, only: [:index, :new, :create, :destroy]
   # You can have the root of your site routed with "root"
-  root to: 'home#index'
+  root to: 'hexes'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
