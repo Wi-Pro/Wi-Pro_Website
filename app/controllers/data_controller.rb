600d42afcd27a1fd58100cfe74603a73c6e84c2d
kevinbird15 <kevinbird15@gmail.com>
2015-03-07 01:08:12 -0600

    trying query strings

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -2,7 +2,7 @@ class DataController < ApplicationController
   def index
     fname = "data_file.txt"
     fd = File.open(fname, "w")
-    fd.puts "Am I there?"
+    fd.puts params[:t]
     fd.close
   end
 end
