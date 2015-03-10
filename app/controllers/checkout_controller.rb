153c63cad8493444e69f587c1a1da638f24e22ff
kevinbird15 <kevinbird15@gmail.com>
2015-03-10 01:22:10 -0500

    Revert "committing to roll back"
    This reverts commit 92f51e0098eff9c712c3195ca951d11e371e65d3.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,10 +1,5 @@
 class CheckoutController < ApplicationController
   def index
     @hex = Hex.new
-    if @hex.save
-      redirect_to hexes_path, success: "Wi-Pro Checked Out!"
-    else
-      render "index"
-    end
   end
 end
