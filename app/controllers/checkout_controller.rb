92f51e0098eff9c712c3195ca951d11e371e65d3
kevinbird15 <kevinbird15@gmail.com>
2015-03-10 01:21:54 -0500

    committing to roll back

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,10 @@
 class CheckoutController < ApplicationController
   def index
     @hex = Hex.new
+    if @hex.save
+      redirect_to hexes_path, success: "Wi-Pro Checked Out!"
+    else
+      render "index"
+    end
   end
 end
