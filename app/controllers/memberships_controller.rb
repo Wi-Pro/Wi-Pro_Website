2a0f1f24d4797b1df550b0606279cc71a05b9b6d
kevinbird15 <kevinbird15@gmail.com>
2015-03-10 20:23:25 -0500

    adding memberships

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,2 +1,20 @@
 class MembershipsController < ApplicationController
+  def index
+    @groups = Groups.all
+    @users = Users.all
+    @membership = Membership.new
+  end
+  def create
+    @membership = Membership.new(membership_params)
+    if @membership.save
+      redirect_to "/membership", success: "#{@membership.userid} has been added to #{@membership.groupid}"
+    else
+      render "index"
+    end
+  end
+
+private
+  def membership_params
+    params.require(:membership).permit(:groupid, :userid)
+  end
 end
