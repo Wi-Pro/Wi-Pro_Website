29c4283c2d05a927d77330050e184a4bd65f603d
kevinbird15 <kevinbird15@gmail.com>
2015-03-11 00:08:33 -0500

    membership != memberships...again

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -7,7 +7,7 @@ class MembershipsController < ApplicationController
   def create
     @membership = Membership.new(membership_params)
     if @membership.save
-      redirect_to "/membership", success: "#{@membership.userid} has been added to #{@membership.groupid}"
+      redirect_to "/memberships", success: "#{@membership.userid} has been added to #{@membership.groupid}"
     else
       render "index"
     end
