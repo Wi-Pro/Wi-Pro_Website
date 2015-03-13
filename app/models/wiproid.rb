ba61b7f7cf9dae935521f3dece5c835208ad4b4f
kevinbird15 <kevinbird15@gmail.com>
2015-03-13 01:50:48 -0500

    changing vonalidati

--- app/models/wiproid.rb
+++ app/models/wiproid.rb
@@ -1,5 +1,5 @@
 class Wiproid < ActiveRecord::Base
-  validates :name, presence: true
+  #validates :name, presence: true
   validates :wiproid, presence: true, length: {is: 10} #wiproid must be 10 digits long
-  validates :userid, presence: true
+  validates :groupid, presence: true
 end
