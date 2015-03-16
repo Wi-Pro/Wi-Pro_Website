11ea33772b8c42573c7c65da2004ad1c369da8b6
kevinbird15 <kevinbird15@gmail.com>
2015-03-16 16:40:48 -0500

    Checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,6 @@
 class CheckoutController < ApplicationController
   def index
-    @hex = Hex.new
+    @user = User.where(id: current_user.id)
+    @list = Membership.where(userid: current_user.id)
   end
 end
