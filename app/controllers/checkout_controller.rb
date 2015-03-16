3c387c03947596a62839bc50cebd8e68924bc060
kevinbird15 <kevinbird15@gmail.com>
2015-03-16 16:59:14 -0500

    fixing error

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,6 +1,6 @@
 class CheckoutController < ApplicationController
   def index
-    @user = User.where(id: current_user.id).find(params[:id])
+    @user = User.where(id: current_user.id).find(current_user.id)
     @list = Membership.where(userid: current_user.id)
   end
 end
