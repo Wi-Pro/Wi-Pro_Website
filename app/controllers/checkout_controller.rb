5ee2b6889a03a2e7248f29495a8260b4e8c8c87d
kevinbird15 <kevinbird15@gmail.com>
2015-03-17 01:49:58 -0500

    testing

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,5 +2,6 @@ class CheckoutController < ApplicationController
   def index
     @user = User.where(id: current_user.id).find(current_user.id)
     @list = Membership.where(userid: current_user.id)
+    availwp = Hash.new
   end
 end
