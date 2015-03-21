7cf3fa65cdcd7bd3fb9f9ab97abc43aae9029308
kevinbird15 <kevinbird15@gmail.com>
2015-03-21 16:09:49 -0500

    hanging?

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -11,7 +11,7 @@ class CheckoutsController < ApplicationController
       flash[:success] = "Checked out!"
       redirect_to "/hexes"
     else
-      redirect_to "/checkouts"
+      redirect_to "/groups"
     end
   end
 
