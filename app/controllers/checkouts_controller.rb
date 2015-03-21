61dae0f255e04216c9fbeb5acff31b23cfaf1c5a
kevinbird15 <kevinbird15@gmail.com>
2015-03-21 16:08:50 -0500

    testing

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -11,7 +11,7 @@ class CheckoutsController < ApplicationController
       flash[:success] = "Checked out!"
       redirect_to "/hexes"
     else
-      render 'index'
+      redirect_to "/checkouts"
     end
   end
 
