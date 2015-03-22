b1965939a3338f9d909b8a2a12151619627d2bcb
kevinbird15 <kevinbird15@gmail.com>
2015-03-22 00:51:47 -0500

    going to work now

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -2,7 +2,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    hex = Wiproid.where(id: Checkout.where(userid: $globaluserid).last).wiproid
+    hex = Wiproid.find(Checkout.where(userid: $globaluserid).last.wiproid).wiproid
     "uploads/#{model.class.to_s.underscore}/#{hex.wiproid}"
   end
 
