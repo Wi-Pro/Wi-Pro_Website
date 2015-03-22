47264c31b7949f8966233d49edadf413cb7a73a1
kevinbird15 <kevinbird15@gmail.com>
2015-03-21 22:01:57 -0500

    adding device stuff

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,2 +1,28 @@
 class DevicesController < ApplicationController
+  def index
+    @device = Device.new
+  end
+
+  def admin
+    @devices = Device.all
+  end
+
+  def new
+    @device = Device.new
+  end
+
+  def create
+    @device = Device.new(device_params)
+    @device.signature = @device.signature.hex
+    if @device.save
+      flash[:success] = "The device #{@device.name} has been added"
+      redirect_to "/"
+    else
+      render "index"
+    end
+  end
+private
+  def device_params
+    params.require(:device).permit(:name, :signature, :flash_size, :pin_count)
+  end
 end
