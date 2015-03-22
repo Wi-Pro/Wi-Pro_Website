591b15c6b645e87b02c8f2038a04795964885b89
kevinbird15 <kevinbird15@gmail.com>
2015-03-21 22:03:06 -0500

    routes once again

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Myapp::Application.routes.draw do
   resources :groups
   resources :memberships
   resources :checkouts
+  resources :devices
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   #resources :hexes, only: [:index, :new, :create, :destroy]
