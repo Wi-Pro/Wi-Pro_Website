15ac7da4ff1d9d44823492868f3d089433f909db
kevinbird15 <kevinbird15@gmail.com>
2015-03-22 00:41:38 -0500

    adding manual piece

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -2,6 +2,15 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
     hex = Wiproid.where(id: Checkout.where(userid: current_user.id).last.wiproid)
     "uploads/#{model.class.to_s.underscore}/#{hex.wiproid}"
   end
