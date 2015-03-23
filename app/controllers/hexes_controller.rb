076f3314c0143cff9f25551e1c55a61a99606f5a
kevinbird15 <kevinbird15@gmail.com>
2015-03-23 14:00:01 -0500

    fulling validationg

--- app/controllers/hexes_controller.rb
+++ app/controllers/hexes_controller.rb
@@ -19,7 +19,7 @@ class HexesController < ApplicationController
     @hex = Hex.new(hex_params)
     if @hex.save
       flash[:success] = "Programming Initiated!"
-      file = File.open("uploads/hex/#{@hex.wiproid}/deviceinfo.txt", "w+")
+      file = File.open("/home/rails/public/uploads/hex/#{@hex.wiproid}/deviceinfo.txt", "w+")
       file.write("Name: #{Device.find(@hex.deviceid).name}, #{Device.find(@hex.deviceid).signature}, flash: #{Device.find(@hex.deviceid).flash_size}, pins: #{Device.find(@hex.deviceid).pin_count}")
       file.close
       redirect_to hexes_path
