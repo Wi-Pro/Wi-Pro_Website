b28cb185497609ba9336628f80b0aae3acd7766d
kevinbird15 <kevinbird15@gmail.com>
2015-03-26 03:12:19 -0500

    adding ssid

--- app/controllers/ssid_controller.rb
+++ app/controllers/ssid_controller.rb
@@ -1,4 +1,19 @@
 class SsidController < ApplicationController
   def index
+    @ssid = Ssid.new
   end
+  def create
+    @ssid = Ssid.new(ssid_params)
+    if @ssid.save
+      flash[:success] = "Wireless Network Saved!"
+      redirect_to "/"
+    else
+      render "index"
+    end
+  end
+private
+  def ssid_params
+    params.require(:ssid).permit(:wiproid, :ssid, :password)
+  end
+end
 end
