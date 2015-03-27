90bfee52ef41fb82176a68c151948cf21dcd7903
kevinbird15 <kevinbird15@gmail.com>
2015-03-27 17:45:24 -0500

    adding \ns

--- app/controllers/wiproids_controller.rb
+++ app/controllers/wiproids_controller.rb
@@ -26,21 +26,7 @@ class WiproidsController < ApplicationController
       flag_file.write("0 0")#Programming and SSID Scan are the two flags
       flag_file.close
       ssid_file = File.open("/home/rails/public/uploads/hex/#{@wiproid.wiproid}/ssid.txt", "w+")
-      ssid_file = File.write("! 13 networks found
-!  # Ch RSSI MAC (BSSID)       Network (SSID)
-#  0  1  -51 6C:B0:CE:B8:A2:9E Wi-Pro
-#  1  1  -45 20:4E:7F:12:3F:1C Daycare
-#  2  1  -68 68:BD:AB:67:2C:C2 UNOGuest
-#  3  1  -69 00:06:5A:00:EA:51 WiFi Research Network
-#  4  1  -68 68:BD:AB:67:2C:C1 UNOSecure
-#  5  1  -74 68:BD:AB:67:35:92 UNOGuest
-#  6  1  -74 68:BD:AB:67:35:91 UNOSecure
-#  7  1  -69 68:BD:AB:67:2C:C0 eduroam
-#  8 11  -41 30:46:9A:60:7F:08 dickbutt
-#  9 11  -57 68:BD:AB:67:56:52 UNOGuest
-# 10 11  -81 AE:7C:A4:C2:7F:B6 PJ-WIRELESS5
-# 11 11  -57 68:BD:AB:67:56:51 UNOSecure
-# 12 11  -84 68:BD:AB:67:56:50 eduroam")
+      ssid_file = File.write("! 13 networks found\n!  # Ch RSSI MAC (BSSID)       Network (SSID)\n#  0  1  -51 6C:B0:CE:B8:A2:9E Wi-Pro\n#  1  1  -45 20:4E:7F:12:3F:1C Daycare\n#  2  1  -68 68:BD:AB:67:2C:C2 UNOGuest\n#  3  1  -69 00:06:5A:00:EA:51 WiFi Research Network\n#  4  1  -68 68:BD:AB:67:2C:C1 UNOSecure\n#  5  1  -74 68:BD:AB:67:35:92 UNOGuest\n#  6  1  -74 68:BD:AB:67:35:91 UNOSecure\n#  7  1  -69 68:BD:AB:67:2C:C0 eduroam\n#  8 11  -41 30:46:9A:60:7F:08 dickbutt\n#  9 11  -57 68:BD:AB:67:56:52 UNOGuest\n# 10 11  -81 AE:7C:A4:C2:7F:B6 PJ-WIRELESS5\n# 11 11  -57 68:BD:AB:67:56:51 UNOSecure\n# 12 11  -84 68:BD:AB:67:56:50 eduroam\n")
       ssid_file.close
       #`touch /home/rails/public/uploads/hex/#{@wiproid.wiproid}/flagfile.txt`
       #flags_file.close
