9111ec863ca6e4868c58a95ac7a5581c69e0898a
kevinbird15 <kevinbird15@gmail.com>
2015-03-28 00:42:44 -0500

    adding wiporid autofill

--- app/controllers/ssids_controller.rb
+++ app/controllers/ssids_controller.rb
@@ -3,17 +3,19 @@ class SsidsController < ApplicationController
     @ssid = Ssid.new
     @list = Array.new
     regex = /(^!.+|^\S+\s+\d+\s+\d+\s+\S+\s+\S+\s+|\n)/
-    @wiproid = Wiproid.find(Checkout.where(userid: current_user.id).last.wiproid)
-    flags_file = File.open("/home/rails/public/uploads/hex/#{@wiproid.wiproid}/flagfile.txt", "w+")
-    flags_file.write("0 1")
-    flags_file.close
-    @name = File.open("/home/rails/public/uploads/hex/#{@wiproid.wiproid}/ssid.txt", "r")
-    @name.each do |line|
-      line.sub!(regex, "")
-      @list.push(line.tr("\n", ""))
+    if Checkout.where("userid = ?", current_user.id).last
+      @wiproid = Wiproid.find(Checkout.where(userid: current_user.id).last.wiproid)
+      flags_file = File.open("/home/rails/public/uploads/hex/#{@wiproid.wiproid}/flagfile.txt", "w+")
+      flags_file.write("0 1")
+      flags_file.close
+      @name = File.open("/home/rails/public/uploads/hex/#{@wiproid.wiproid}/ssid.txt", "r")
+      @name.each do |line|
+        line.sub!(regex, "")
+        @list.push(line.tr("\n", ""))
+      end
+      @list.reject! { |r| r.empty? }
+      @list.uniq!
     end
-    @list.reject! { |r| r.empty? }
-    @list.uniq!
   end
   def create
     @ssid = Ssid.new(ssid_params)
