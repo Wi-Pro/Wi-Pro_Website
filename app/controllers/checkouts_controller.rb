621cc63899fa64ddc03e147c81457ade7897f6e9
kevinbird15 <kevinbird15@gmail.com>
2015-03-28 01:17:49 -0500

    adding an initial ssid generation on checkout

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -10,8 +10,11 @@ class CheckoutsController < ApplicationController
     @checkout = Checkout.new(checkout_params)
     @checkout.userid = current_user.id
     if @checkout.save
+      flags_file = File.open("/home/rails/public/uploads/hex/#{Wiproid.find(@checkout.wiproid).wiproid}/flagfile.txt", "w+")
+      flags_file.write("0 1")
+      flags_file.close
       flash[:success] = "Checked out!"
-      redirect_to "/hexes"
+      redirect_to "/"
     else
       render 'index'
     end
