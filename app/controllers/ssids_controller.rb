ea77b99f837d33257d34cb02524433a1d24eebb4
kevinbird15 <kevinbird15@gmail.com>
2015-03-27 21:29:50 -0500

    trying to get rid of the new lines

--- app/controllers/ssids_controller.rb
+++ app/controllers/ssids_controller.rb
@@ -2,7 +2,7 @@ class SsidsController < ApplicationController
   def index
     @ssid = Ssid.new
     @list = Array.new
-    regex = /(^!.+|^\S+\s+\d+\s+\d+\s+\S+\s+\S+\s+)/
+    regex = /(^!.+\n|\n^\S+\s+\d+\s+\d+\s+\S+\s+\S+\s+)/
     @wiproid = Wiproid.find(Checkout.where(userid: current_user.id).last.wiproid)
     @name = File.open("/home/rails/public/uploads/hex/#{@wiproid.wiproid}/ssid.txt", "r")
     @name.each do |line|
