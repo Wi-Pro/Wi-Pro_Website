e7f62817af3a0cc68d2201936ad13fe0abe1b86c
kevinbird15 <kevinbird15@gmail.com>
2015-03-29 00:03:01 -0500

    delete it til it works

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -16,10 +16,6 @@ class AttachmentUploader < CarrierWave::Uploader::Base
 
   def size_range
     flash_size = Device.find(Hex.last.deviceid).flash_size
-    if flash_size < 65535
       0...flash_size
-    else
-      0...65535
-    end
   end
 end
