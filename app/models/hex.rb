a5a5a47228f06f01ace70993ba229c66cafb15be
kevinbird15 <kevinbird15@gmail.com>
2015-03-29 00:24:51 -0500

    living on a prayer

--- app/models/hex.rb
+++ app/models/hex.rb
@@ -4,7 +4,7 @@ class Hex < ActiveRecord::Base
   validates :attachment,
     presence: true,
     :file_size => {
-      :maximum => Device.find(Hex.last.deviceid).flash_size
+      :maximum => Device.find(@hex.deviceid).flash_size
     }
   #validates :wiproid, presence: true, length: {is: 10}
   validates :deviceid, presence: true
