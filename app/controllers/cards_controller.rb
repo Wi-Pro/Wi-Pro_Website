0f57605dec779d80957d12dc59d9236f44410ba0
kevinbird15 <kevinbird15@gmail.com>
2015-04-07 23:54:22 -0500

    know what @ does now

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,11 +1,11 @@
 class CardsController < ApplicationController
   def index
-    deck = Array.new(52)
+    @deck = Array.new(52)
     i = 1
     j = 0
     4.times do
       13.times do
-        deck[j] = i
+        @deck[j] = i
         j+=1
         i+=1
       end
