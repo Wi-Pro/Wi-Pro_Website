cb86859973d5ae78df1b03f906fc56c46ddcdb1a
kevinbird15 <kevinbird15@gmail.com>
2015-04-07 23:56:40 -0500

    use 52 numbers

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,13 +3,18 @@ class CardsController < ApplicationController
     @deck = Array.new(52)
     i = 1
     j = 0
-    4.times do
-      13.times do
-        @deck[j] = i
-        j+=1
-        i+=1
-      end
-      i=1
+    #4.times do
+    #  13.times do
+    #    @deck[j] = i
+    #    j+=1
+    #    i+=1
+    #  end
+    #  i=1
+    #end
+    52.times do
+      @deck[j] = i
+      j+=1
+      i+=1
     end
   end
 end
