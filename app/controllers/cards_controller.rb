2474f4b404b9978a3086dcdf9b2fcb29846d01cb
kevinbird15 <kevinbird15@gmail.com>
2015-04-07 23:53:17 -0500

    no ++

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -6,8 +6,8 @@ class CardsController < ApplicationController
     4.times do
       13.times do
         deck[j] = i
-        j++
-        i++
+        j+=1
+        i+=1
       end
       i=1
     end
