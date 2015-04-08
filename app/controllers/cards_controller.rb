2d40c425fd432c5c063e95d9eff49d2d4463310d
kevinbird15 <kevinbird15@gmail.com>
2015-04-08 00:12:47 -0500

    trying to add suit

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,21 +1,24 @@
 class CardsController < ApplicationController
   def index
     @deck = Array.new(52)
+    letter = [H, C, S, D]
     i = 1
     j = 0
-    #4.times do
-    #  13.times do
-    #    @deck[j] = i
-    #    j+=1
-    #    i+=1
-    #  end
-    #  i=1
-    #end
-    52.times do
-      @deck[j] = i
-      j+=1
-      i+=1
+    suit = 0
+    4.times do
+      13.times do
+        @deck[j] = i + letter[suit]
+        j+=1
+        i+=1
+      end
+      i=1
+      suit +=1
     end
+    #52.times do
+    #  @deck[j] = i
+    #  j+=1
+    #  i+=1
+    #end
     @original_deck = @deck
     i = 1
     j = 0
